a674440dffaf8a7409816b680bedd2d2a578c5f0
m-fadil <fadhilaikhsani@yahoo.com>
2024-03-22 14:07:27 +0700

    tambah waktu

--- commands/epic.js
+++ commands/epic.js
@@ -15,9 +15,15 @@ const Epic = {
 		);
 
 		for (const game of freeGames) {
+			const date = new Date(game.promotions.promotionalOffers[0].promotionalOffers[0].endDate)
+			const tanggal = date.toLocaleDateString('id-ID', { day: 'numeric', month: 'long', year: 'numeric' });
+			const jam = date.toLocaleTimeString('id-ID', { hour: '2-digit', minute: '2-digit' });
+
+			const caption = `*${game.title}*\n\nGratis hingga ${tanggal}, ${jam} WIB\n\nLink:\nhttps://store.epicgames.com/en-US/p/${game.catalogNs.mappings[0].pageSlug}`;
+
 			await sendWithTyping(args, {
 				image: { url: game.keyImages.find((img) => img.type == 'Thumbnail').url },
-				caption: `*${game.title}*\n\nLink:\nhttps://store.epicgames.com/en-US/p/${game.catalogNs.mappings[0].pageSlug}`,
+				caption,
 			});
 		}
 	},
